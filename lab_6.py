441a690f89d4582f902b1255cb891ec0776dbd63
Ana Bondari <98388544+annbyy@users.noreply.github.com>
2023-12-11 01:06:20 +0200

    Add files via upload
    Добавление дополнительных тестовых кейсов:
    1. Тестирование get_weather для некорректного города
    2. Дополнительные тесты для format_weather

--- lab_6.py
+++ lab_6.py
@@ -10,6 +10,11 @@ class TestWeatherUtils(unittest.TestCase):
         self.assertIsInstance(weather_data, dict)
         self.assertIn('name', weather_data)
 
+        weather_data_invalid = get_weather('InvalidCity')
+        self.assertIsInstance(weather_data_invalid, dict)
+        self.assertIn('cod', weather_data_invalid)
+        self.assertEqual(weather_data_invalid['cod'], '404')
+
     def test_format_weather(self):
         sample_weather_data = {
             'name': 'Stockholm',
@@ -20,16 +25,21 @@ class TestWeatherUtils(unittest.TestCase):
         expected_output = "Now in the city Stockholm Clear (clear sky) and temperature is 0.00°C."
         self.assertEqual(formatted_weather, expected_output)
 
-    @patch('builtins.input', return_value='NonexistentCity')
-    def test_get_weather_nonexistent_city(self, mock_input):
-        weather_data = get_weather('NonexistentCity')
-        self.assertIsInstance(weather_data, dict)
-        self.assertIn('cod', weather_data)
-        self.assertEqual(weather_data['cod'], '404')
+        sample_weather_data_invalid = {
+            'weather': [{'main': 'Clear', 'description': 'clear sky'}],
+            'main': {'temp': 273.15}
+        }
+        formatted_weather_invalid = format_weather(sample_weather_data_invalid)
+        expected_output_invalid = "Error."
+        self.assertEqual(formatted_weather_invalid, expected_output_invalid)
 
-        formatted_weather = format_weather(weather_data)
-        expected_output = "City is not found."
-        self.assertEqual(formatted_weather, expected_output)
+        sample_weather_data_no_main = {
+            'name': 'Stockholm',
+            'weather': [{'main': 'Clear', 'description': 'clear sky'}],
+        }
+        formatted_weather_no_main = format_weather(sample_weather_data_no_main)
+        expected_output_no_main = "Error."
+        self.assertEqual(formatted_weather_no_main, expected_output_no_main)
 
 if __name__ == '__main__':
     unittest.main()
